2b0970357cd425e4591014ab5225929350a9b953
90patrykpopiolek-gif <90patryk.popiolek@gmail.com>
2026-02-18 16:56:41 +0100

    Update File.js

--- models/File.js
+++ models/File.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const FileSchema = new mongoose.Schema({
+  fileId: { type: String, required: true },
+  chatId: { type: String, required: true },
+  path: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("File", FileSchema);
 
