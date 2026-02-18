78c20622ef9a30b9a6cdac3d5f0e26bbb457fd99
90patrykpopiolek-gif <90patryk.popiolek@gmail.com>
2026-02-18 16:54:59 +0100

    Update ChatSession.js

--- models/ChatSession.js
+++ models/ChatSession.js
@@ -4,7 +4,9 @@ const ChatSessionSchema = new mongoose.Schema({
   chatId: { type: String, required: true, unique: true },
   userId: { type: String, required: true },
   title: { type: String, required: true },
-  lastUsedAt: { type: Date, default: Date.now }
+  lastUsedAt: { type: Date, default: Date.now },
+
+  activeFileId: { type: String, default: null }
 });
 
 module.exports = mongoose.model("ChatSession", ChatSessionSchema);
