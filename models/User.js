42b280199a5c6e60935efd3a206723dc9f98704f
90patrykpopiolek-gif <90patryk.popiolek@gmail.com>
2026-03-04 15:08:08 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   _id: String, // Firebase UID
-  credits: { type: Number, default: 0 }
+
+  // Kredyty użytkownika
+  credits: { type: Number, default: 0 },
+
+  // Limity dzienne
+  limitGenerateUsed: { type: Number, default: 0 },   // generowanie obrazów (limit 2)
+  limitPhotoUsed: { type: Number, default: 0 },      // zdjęcia/aparat (limit 5)
+  limitDocumentsUsed: { type: Number, default: 0 },  // dokumenty (limit 3)
+
+  // Data ostatniego resetu limitów
+  lastLimitsReset: { type: Date, default: null }
 });
 
 module.exports = mongoose.model("User", UserSchema);
+
 
