89d4879af3b65ad95c510676dfd0ea59ce29dfd0
90patrykpopiolek-gif <90patryk.popiolek@gmail.com>
2026-03-22 18:13:13 +0100

    Update DocumentChunk.js

--- models/DocumentChunk.js
+++ models/DocumentChunk.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const DocumentChunkSchema = new mongoose.Schema({
-  chatId: { type: String, required: true },
-  text: { type: String, required: true },
-  embedding: { type: [Number], required: true }
+  chatId: String,
+  userId: String,
+  documentId: String,
+  chunkIndex: Number,
+  text: String,
+  embedding: [Number]
 });
 
 module.exports = mongoose.model("DocumentChunk", DocumentChunkSchema);
+
